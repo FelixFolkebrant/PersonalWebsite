c6d8beaf819c84d1b00788d1d0caf858053d89ca
QXPP <Felix.folkebrant@gmail.com>
2021-12-05 15:43:31 +0100

    Logo animaion
    Finally added the logo animation, took forever. Had problems with the lottie library interpreting the file. After experimenting I have come to the conclusion that a https://lottiefiles.com/preview export is the best way to aproach the problem. The problem lies within the lottie.js player and not the bodymovin plugin in aftereffects (even though you can't preview) so use the lottiefiles.com link

---
+++ src/script.js
@@ -0,0 +1,16 @@
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+
+// // Usage
+// sleep(500).then(() => {
+//     //   code
+// });
+sleep(5000).then(() => {
+    document.getElementById("logoNeumorph").style.opacity = "1";
+    console.log("Changed Opacity")
+});
+sleep(7000).then(() => {
+    document.getElementById("LogoAnim").style.opacity = "0";
+    console.log("Changed Opacity")
+});
